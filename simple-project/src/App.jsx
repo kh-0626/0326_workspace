fc7bd8ccc96748ee8d11ee8404b8db41e78b6459
kh-0626 <kdw052516@naver.com>
2025-03-26 11:54:15 +0900

    20250326

---
+++ simple-project/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>고게 다예요</h1>
+
+      <p>요기다가 이제 HTML이랑 CSS랑 작성하면 끝</p>
+
+      <h6>이게 리엑트다</h6>
+    
+    
+    </>
+  );
+}
+
+export default App
